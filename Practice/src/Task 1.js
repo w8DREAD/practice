01194287348dff1ca06e3d5bc39157712a60be56
w8DREAD <dreadgodik@gmail.com>
2019-06-05 18:26:48 +0300

    не понял  5го задания

---
+++ Practice/src/Task 1.js
@@ -0,0 +1,7 @@
+var arr = [1, 2];
+
+function pick(arg) {
+console.log(arg[Math.round(Math.random()*(arg.length - 1))])
+}
+
+pick(arr);
---
+++ Practice/src/Task 6.js
@@ -0,0 +1,35 @@
+var arrName = ["Murka", "Barsik", "Lidka", "Zinka", "Tima", "Simba", "Durka", "Glasha", "Scot", "Scotina"];
+var arrAge = [2, 3, 4, 5, 6, 7, 8, 9, 10, 11, 12];
+var arrGender = ["Male", "Female"];
+var arrLegsCount = [1, 2, 3, 4];
+var arrTailLength = [10, 11, 12, 13, 14, 15, 16, 17, 18, 19, 20];
+
+function pick(arg) {
+    return arg[Math.round(Math.random()*(arg.length - 1))]
+}
+
+function catFactory(arg) {
+
+    var cat = {
+        name: pick(arrName),
+        age: pick(arrAge),
+        gender: pick(arrGender),
+        legsCount: pick(arrLegsCount),
+        tailLength: pick(arrTailLength)
+    };
+    for (i=0; i < arguments.length; i++) {
+
+        for (var key in arguments[i]) {
+            for (var prop in cat) {
+                if (key == prop) {
+                    cat[key] = arguments[i][key]
+                }
+            }
+        }
+    }
+
+    return cat;
+
+}
+
+console.log(catFactory({gender: "Male"}, {name: "Barsik"}));
