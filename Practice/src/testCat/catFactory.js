4c245509a7604a6f16447f1b9caf507dbcd9b08b
w8DREAD <dreadgodik@gmail.com>
2019-06-07 14:26:12 +0300

    second

--- Practice/src/testCat/catFactory.js
+++ Practice/src/testCat/catFactory.js
@@ -1,8 +1,18 @@
+var assert = require('assert');
+
 var arrName = ["Murka", "Barsik", "Lidka", "Zinka", "Tima", "Simba", "Durka", "Glasha", "Scot", "Scotina"];
 var arrAge = [2, 3, 4, 5, 6, 7, 8, 9, 10, 11, 12];
 var arrGender = ["Male", "Female"];
 var arrLegsCount = [1, 2, 3, 4];
 var arrTailLength = [10, 11, 12, 13, 14, 15, 16, 17, 18, 19, 20];
+
+
+describe('Проверка функции catFactory', () => {
+
+    it('Проверка свойства name', () => {
+        assert("loudness" == Object.keys(catFactory())[0]);
+    });
+});
 
 function pick(arg) {
     return arg[Math.round(Math.random()*(arg.length - 1))]
